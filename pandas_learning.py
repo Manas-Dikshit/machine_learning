f2f2e13f7160946a841d6c55dd13310c302c770e
Manas-Dikshit <manasdikshit48@gmail.com>
2026-04-30 23:05:51 +0530

    pandas started

---
+++ pandas_learning.py
@@ -0,0 +1,13 @@
+#Pandas is a python library used in data analysis, data science, machine learning.
+# series = 1D array
+# DataFrame = 2D array
+# Pandas is built on top of NumPy and provides additional functionality for data manipulation and analysis.
+#We can import, display, and manipulate data using pandas.
+
+import pandas as pd
+
+print(pd.__version__) #to print pandas version
+
+#Series
+# A Series is a one-dimensional labeled array that can hold any data type (integers, strings, floating-point numbers, etc.).
+# Each element in a Series is associated with an index, which can be used to access and manipulate the data.
