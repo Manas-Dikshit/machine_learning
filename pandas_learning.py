abfdf274e332139eb2ffbdd6b5c786144e38496f
Manas-Dikshit <manasdikshit48@gmail.com>
2026-05-01 20:36:15 +0530

    Dataframes

--- pandas_learning.py
+++ pandas_learning.py
@@ -41,3 +41,31 @@ calories_series = pd.Series(calories)
 calories_series.loc["day2"] += 2000 #modifying element using custom label
 print(calories_series)
 print(calories_series[calories_series > 2000]) #filtering elements greater than 2000
+
+
+#DataFrame
+# A DataFrame is a two-dimensional labeled data structure that can hold data of different types (like a spreadsheet or SQL table).
+# It consists of rows and columns, where each column can be of a different data type (e.g., integers, floats, strings).
+# DataFrames are widely used for data manipulation, analysis, and visualization.
+#A tabular data structure that organizes data into rows and columns, similar to a spreadsheet or SQL table. Each column can contain different types of data (e.g., integers, floats, strings), and each row represents a record or observation.
+#DataFrames are built on top of Series, where each column in a DataFrame is essentially a Series.
+
+data = {
+    "Name": ["Alice", "Bob", "Charlie", "David"],"Age": [25, 30, 35, 40],}
+df = pd.DataFrame(data)
+df_labels = pd.DataFrame(data, index = ['a', 'b', 'c', 'd'])
+print(df)
+print(df_labels)
+print(df_labels.loc["a"]) #accessing row using custom label
+print(df.iloc[0]) #accessing row using integer position
+print(df_labels["Name"]) #accessing column using column name
+print(df_labels["Age"]) #accessing column using column name
+
+# Add a new Coloumn
+df_labels["City"] = ["New York", "Los Angeles", "Chicago", "Houston"] #adding new column to DataFrame
+print(df_labels)
+
+# Add a new Row
+new_row = pd.DataFrame([{"Name": "Eve", "Age": 28, "City": "Phoenix"}], index=["e"]) #creating new row as a DataFrame
+df_labels = pd.concat([df_labels, new_row], ignore_index=True) #adding new row to DataFrame
+print(df_labels)
